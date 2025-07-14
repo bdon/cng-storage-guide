211bf819af85d058b5c2488698bc8497dba8f7a1
Brandon Liu <bdon@bdon.org>
2025-07-14 14:48:28 +0800

    add basic interactivity

--- src/components/CostComponent.tsx
+++ src/components/CostComponent.tsx
@@ -1,7 +1,30 @@
+import { For, type JSX, createSignal } from "solid-js";
+
 function CostComponent(props: any) {
-  console.log(props.providers);
+  const providers = props.providers;
+
+  const [selectedProvider, setSelectedProvider] = createSignal(providers[0]);
+
+  console.log(selectedProvider());
+
+  const selectProvider: JSX.EventHandler<HTMLSelectElement, Event> = (event) => {
+    const slug = event.currentTarget.value;
+    console.log(slug);
+    setSelectedProvider(providers.find(item => item.id === slug));
+  };
+
   return (
-    <div>hello</div>
+    <div>
+      <select onChange={selectProvider}>
+       <For each={props.providers}>
+          {(provider) => (
+            <option value={provider.id}>{provider.data.name}</option>
+          )}
+        </For>
+      </select>
+      <p>Total storage costs for 100GB: {selectedProvider().data.cost_per_gb_stored * 100}</p>
+      <p>Total egress costs for 100GB: {selectedProvider().data.cost_per_gb_egress * 100}</p>
+    </div>
   );
 }
 
